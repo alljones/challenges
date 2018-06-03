616c05b555645899d9e0cc55265fa94102f5a441
Allan Jones <iamallanjones@gmail.com>
2018-06-03 12:18:51 -0500

    Clean up logic with forEach

--- todolist/list.js
+++ todolist/list.js
@@ -1,19 +1,49 @@
 var todos = [];
 var input = prompt("What would you like to do?");
 
-
-
+// = = = = = = = = = =  
+// LOGIC
+// = = = = = = = = = =  
 while (input !== "quit") {
     //handle input
     if (input === "list") {
-        console.log(todos);
+        listTodos();
+      
     } else (input === "new"){
-        // ask for new todo
-        var newTodo = prompt("Enter new todo");
-        //add to todo array
-        todos.push(newTodo);
-    }
+        addTodos();
+    } else if (input ==="delete"){
+        deleteTodos();
+}
+console.log("Ok, see you next time!");
+
+// = = = = = = = = = =  
+// FUNCTIONS
+// = = = = = = = = = =  
+
+function listTodos(){
+    console.log("***********");
+    todos.forEach(function(todo, i){
+            console.log(i + ":" + todos);  
+        });
+    console.log("***********");
+
+}
+
+function addTodos(){
+    // ask for new todo
+    var newTodo = prompt("Enter new todo");
+    //add to todo array
+    todos.push(newTodo);
+    console.log("Added Todo");
+}
+
+function deleteTodos(){
+    //ask for index of todo to be deleted
+    var index = prompt("Enter index of todo to delete");
+    //delete that todo
+    //splice() -- Remove at index enter/ only 1 item
+    todos.splice(index, 1); 
+    console.log("Deleted Todo");
     //ask again for new input
     input = prompt("What would you like to do?");
 }
-console.log("Ok, see you next time!")
